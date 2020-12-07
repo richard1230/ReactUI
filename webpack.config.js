c80cefba405a020774fe6559bd5cde31b991bd48
richard1230 <yongjinhu1230@gmail.com>
2020-12-07 18:24:37 +0800

    index.tsx 打包成 index.js 了！

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+module.exports = {
+  mode: 'production',
+  //输入, 这是入口文件
+  entry: {
+    index: './lib/index.tsx'
+  },
+  //输出:
+  output: {
+    //__dirname表示当前目录,dist是我们要去的目录
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'FUI',
+    //输出格式
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules:[
+      {
+        // \ 为转义符号
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+
+}

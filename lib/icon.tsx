4aab0606277910a441a0aeadbb60a6d1a6af0290
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 22:30:52 +0800

    icon1+注释

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,6 +4,9 @@ interface IconProps {
     name: string
 }
 
+//如何申明一个函数组件接受的props类型
+//<>：表示FunctionComponent接受一个参数,其源代码里面指明了此参数为p(这里为name),不传的话即为空对象
+// interface FunctionComponent<P = {}>
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
     return (
         <span>{props.name}</span>

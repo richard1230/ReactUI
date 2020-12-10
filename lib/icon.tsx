4384c6bc98147fd4479d9bc48295b80290749294
richard1230 <yongjinhu1230@gmail.com>
2020-12-10 15:10:36 +0800

    使用解构赋值

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -12,13 +12,13 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
 //如何申明一个函数组件接受的props类型
 //<>：表示FunctionComponent接受一个参数,其源代码里面指明了此参数为p(这里为name),不传的话即为空对象
 // interface FunctionComponent<P = {}>
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-    const {className, ...restProps} = props;
+const Icon: React.FunctionComponent<IconProps> =
+    ({className,name, ...restProps} ) => {
     return (
         <svg className={classes('fui-icon', className)}
              {...restProps}
         >
-            <use xlinkHref={`#${props.name}`}/>
+            <use xlinkHref={`#${name}`}/>
         </svg>
 
     )

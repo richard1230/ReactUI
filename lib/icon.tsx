c2f89794950aca5f799a528e03029143769bf5a4
richard1230 <yongjinhu1230@gmail.com>
2020-12-10 13:14:09 +0800

    添加 CSS

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import  "./importIcons"
-
-
-
-
+import "./importIcons"
+import './icon.scss';
 
 
 interface IconProps {
@@ -13,14 +10,12 @@ interface IconProps {
 //如何申明一个函数组件接受的props类型
 //<>：表示FunctionComponent接受一个参数,其源代码里面指明了此参数为p(这里为name),不传的话即为空对象
 // interface FunctionComponent<P = {}>
-const Icon:React.FunctionComponent<IconProps> = (props)=>{
+const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <span>
-            <svg>
-                <use xlinkHref={`#${props.name}`}/>
-            </svg>
+        <svg className="fui-icon">
+            <use xlinkHref={`#${props.name}`}/>
+        </svg>
 
-        </span>
     )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-sprite-loader',
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   },
 

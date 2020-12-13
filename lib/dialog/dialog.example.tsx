1cc37747d199aba2a668ac7ac207bc6d26f446ab
richard1230 <yongjinhu1230@gmail.com>
2020-12-13 22:10:53 +0800

    dialog 1

---
+++ lib/dialog/dialog.example.tsx
@@ -0,0 +1,11 @@
+import React ,{useState}from "react";
+import Dialog from "./dialog";
+export default ()=>{
+    const [x,setX] = useState(false)
+    return(
+        <div>
+            <button onClick={()=>setX(!x)}>click</button>
+            <Dialog visible={x}/>
+        </div>
+    )
+}

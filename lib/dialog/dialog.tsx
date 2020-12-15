648733d25405b73bacac809989fac10dc9a387c0
richard1230 <yongjinhu1230@gmail.com>
2020-12-16 00:33:37 +0800

    scopedClassMaker终极重构+单元测试

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,7 +1,7 @@
 import React, {Fragment, ReactElement, ReactNode} from "react";
 import './dialog.scss'
 import {Icon} from "../index";
-import scopedClassMaker from '../classes'
+import {scopedClassMaker} from "../helpers/classes";
 import ReactDOM from "react-dom";
 
 interface Props {
--- lib/layout/aside.tsx
+++ lib/layout/aside.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import scopedClassMaker from "../classes";
+import {scopedClassMaker} from "../helpers/classes";
 
 const sc = scopedClassMaker('gu-layout')
 
@@ -10,7 +10,7 @@ const  Aside: React.FunctionComponent<Props> = (props) => {
     const {className, ...rest}= props;
     return (
         <div className={sc('aside',{extra:className})} {...rest}>
-            aside
+        aside
         </div>
     )
 };

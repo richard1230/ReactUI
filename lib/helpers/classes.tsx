5335ac5f08d4a08fc30e65dd3f543bc130b86aa3
richard1230 <yongjinhu1230@gmail.com>
2020-12-16 11:11:23 +0800

    classes.tsx重构3

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -20,15 +20,14 @@ function scopedClassMaker(prefix: string) {
         let result;
         if (typeof name === 'string' || name === undefined) {
             name2 = [name]
-            result = name2.map(name=>
-                [prefix, name].filter(Boolean).join('-')).join(' ')
+
         } else {//此时为name为ClassToggles类型
             name2 = Object.entries(name).filter(k => k[1]).map(kv => kv[0]);
-
-            result = name2.map(name =>
-                 [prefix, name].filter(Boolean).join('-')
-            ).join(' ')
         }
+
+        result = name2.map(name =>
+            [prefix, name].filter(Boolean).join('-')
+        ).join(' ')
 
         if (options && options.extra) {
             return [result, options && options.extra].filter(Boolean).join(' ');

fcf49cf07bc0a813bd433866fbc00591866d57f8
richard1230 <yongjinhu1230@gmail.com>
2020-12-16 12:38:38 +0800

    layout组件基本完成

--- lib/layout/header.tsx
+++ lib/layout/header.tsx
@@ -3,10 +3,14 @@ import {scopedClassMaker} from "../helpers/classes";
 
 const sc = scopedClassMaker('gu-layout')
 
+//继承了HTMLAttributes这个对象，后面用Layout组件的时候就可以接受classname和style了
+interface Props extends React.HTMLAttributes<HTMLElement>{
 
-const Header: React.FunctionComponent = () => {
+}
+const Header: React.FunctionComponent<Props> = (props) => {
+    const {className, ...rest}= props;
     return (
-        <div className={sc('header')}>
+        <div className={sc('content',{extra:className})} {...rest}>
             header
         </div>
     )

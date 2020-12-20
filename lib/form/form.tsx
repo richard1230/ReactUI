a5804a695888a3964a4ed9d502b3373901060f5e
richard1230 <yongjinhu1230@gmail.com>
2020-12-20 14:46:28 +0800

    表单基本对齐

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,44 +1,58 @@
 import * as React from "react";
 import {ReactFragment} from "react";
 import Input from "../input/input";
+import classes from "../helpers/classes";
+import './form.scss'
 
-export interface FormValue{
-   [K:string]:any
+export interface FormValue {
+    [K: string]: any
 }
 
 interface Props {
-    value:FormValue;
-    fields:Array<{name:string,label:string,input:{type:string}}>;
-    buttons:ReactFragment;
-    onSubmit:React.FormEventHandler;
-    onChange:(value:FormValue)=>void;
-    errors:{[K:string]:string[]}
+    value: FormValue;
+    fields: Array<{ name: string, label: string, input: { type: string } }>;
+    buttons: ReactFragment;
+    onSubmit: React.FormEventHandler;
+    onChange: (value: FormValue) => void;
+    errors: { [K: string]: string[] }
 }
 
-const Form:React.FunctionComponent<Props>=(props)=>{
-    const onSubmit:React.FormEventHandler<HTMLFormElement> = (e)=>{
+const Form: React.FunctionComponent<Props> = (props) => {
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
         props.onSubmit(e);
     }
-    const onInputChange = (name:string,value:string)=>{
-      const  newFormValue = {...formData,[name]:value}
+    const onInputChange = (name: string, value: string) => {
+        const newFormValue = {...formData, [name]: value}
         props.onChange(newFormValue)
     }
     const formData = props.value
-    console.log("formData: "+formData);
-    return(
+    console.log("formData: " + formData);
+    return (
         <form onSubmit={onSubmit}>
-            {props.fields.map(f=>
-                <div key={f.name}>
-                    {f.label}
-                    <Input typeof={f.input.type} value={formData[f.name]}
-                           onChange={(e)=>onInputChange(f.name,e.target.value)}
-                    />
-                    <div>{props.errors[f.name]}</div>
-            </div>)}
-            <div>
-                {props.buttons}
-            </div>
+            <table>
+                {props.fields.map(f =>
+
+                    <tr className={classes('fui-form-row')} key={f.name}>
+                        <td>
+                          <span >
+                         {f.label}
+                          </span>
+                        </td>
+                        <td>
+                            <Input typeof={f.input.type} value={formData[f.name]}
+                                   onChange={(e) => onInputChange(f.name, e.target.value)}
+                            />
+                            <div>{props.errors[f.name]}</div>
+                        </td>
+
+
+                    </tr>
+                )}
+                <div>
+                    {props.buttons}
+                </div>
+            </table>
         </form>
     )
 }

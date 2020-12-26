a453db6af3f76033130b19478ab743ace63fb3c0
richard1230 <yongjinhu1230@gmail.com>
2020-12-26 22:45:31 +0800

    滚动条可以动了

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -10,9 +10,15 @@ interface Props  extends HTMLAttributes<HTMLDivElement>{
 const Scroll:React.FunctionComponent<Props> = (props)=>{
     const {children,...rest} = props;
     const [barHeight ,setBarHeight] = useState(0)
+    const [barTop,setBarTop]=useState(0)
 
     const onScroll: UIEventHandler =(e)=> {
-        // console.log("e.currentTarget.scrollTop：");
+        const {current} = containerRef;
+        const scrollHeight = current!.scrollHeight;
+        const viewHeight = current!.getBoundingClientRect().height;
+        const scrollTop = current!.scrollTop;
+        setBarTop(scrollTop * viewHeight / scrollHeight);
+
     }
 
     const containerRef = useRef<HTMLDivElement>(null)
@@ -36,7 +42,7 @@ const Scroll:React.FunctionComponent<Props> = (props)=>{
                 {children}
             </div>
             <div className="fui-scroll-track">
-                <div className="fui-scroll-bar" style={{height:barHeight}}>
+                <div className="fui-scroll-bar" style={{height:barHeight, top:barTop}}>
 
                 </div>
 

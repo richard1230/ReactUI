9b64e5ae7b3bf329e5c8055c4091a3c6017a078a
richard1230 <yongjinhu1230@gmail.com>
2020-12-28 00:22:05 +0800

    滚动条与内容同步了

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,31 +1,32 @@
 import * as React from "react";
 import {HTMLAttributes, MouseEventHandler, UIEventHandler, useEffect, useRef, useState} from "react";
 import './scroll.scss'
+
 // import scrollbarWidth from "./scrollbar-width";
 
-interface Props  extends HTMLAttributes<HTMLDivElement>{
+interface Props extends HTMLAttributes<HTMLDivElement> {
 
 }
 
-const Scroll:React.FunctionComponent<Props> = (props)=>{
-    const {children,...rest} = props;
-    const [barHeight ,setBarHeight] = useState(0)
-    const [barTop,_setBarTop]=useState(0)
+const Scroll: React.FunctionComponent<Props> = (props) => {
+    const {children, ...rest} = props;
+    const [barHeight, setBarHeight] = useState(0)
+    const [barTop, _setBarTop] = useState(0)
 
-    const setBarTop = (number:number)=>{
+    const setBarTop = (number: number) => {
         const {current} = containerRef;
         const scrollHeight = current!.scrollHeight;
         const viewHeight = current!.getBoundingClientRect().height;
-        const maxBarTop = (scrollHeight - viewHeight) *  viewHeight / scrollHeight
-        if (number<0) return;
-        if (number>maxBarTop){
+        const maxBarTop = (scrollHeight - viewHeight) * viewHeight / scrollHeight
+        if (number < 0) return;
+        if (number > maxBarTop) {
             number = maxBarTop
         }
         _setBarTop(number)
 
     }
 
-    const onScroll: UIEventHandler =(e)=> {
+    const onScroll: UIEventHandler = (e) => {
         const {current} = containerRef;
         const scrollHeight = current!.scrollHeight;
         const viewHeight = current!.getBoundingClientRect().height;
@@ -35,62 +36,65 @@ const Scroll:React.FunctionComponent<Props> = (props)=>{
     }
 
     const containerRef = useRef<HTMLDivElement>(null)
-    useEffect(()=>{
+    useEffect(() => {
         //这里价格！其实就是告诉ts这里不用管containerRef.current可能为null了，
         // 其实此时他不可能为空，只不过ts不知道
         const scrollHeight = containerRef.current!.scrollHeight;
-        console.log("scrollHeight:  "+scrollHeight);
+        console.log("scrollHeight:  " + scrollHeight);
         // console.log("e.currentTarget.getBoundingClientRect().height: ");
         const viewHeight = containerRef.current!.getBoundingClientRect().height
-        console.log("viewHeight: "+viewHeight);
+        console.log("viewHeight: " + viewHeight);
         // const barHeight = e.currentTarget.getBoundingClientRect().height
-        setBarHeight( viewHeight * viewHeight / scrollHeight)
-    },[])
+        setBarHeight(viewHeight * viewHeight / scrollHeight)
+    }, [])
 
     const draggingRef = useRef(false);
     const firstYRef = useRef(0);
     const firstBarTopRef = useRef(0);
 
-    const onMouseDownBar:MouseEventHandler = (e)=>{
-        draggingRef.current =true;
+    const onMouseDownBar: MouseEventHandler = (e) => {
+        draggingRef.current = true;
         // console.log(e.clientY);
         firstYRef.current = e.clientY;
         firstBarTopRef.current = barTop
         console.log('start');
     };
-    const onMouseMoveBar = (e:MouseEvent )=>{
-         if (draggingRef.current){
-             const delta = e.clientY - firstYRef.current
-             console.log(delta);
-             // console.log('试图移动bar');
-              setBarTop(firstBarTopRef.current+delta)
-         }
+    const onMouseMoveBar = (e: MouseEvent) => {
+        if (draggingRef.current) {
+            const delta = e.clientY - firstYRef.current
+            const newBarTop = firstBarTopRef.current + delta
+            // console.log('试图移动bar');
+            setBarTop(newBarTop)
+            const scrollHeight = containerRef.current!.scrollHeight;
+            const viewHeight = containerRef.current!.getBoundingClientRect().height
+            containerRef.current!.scrollTop = newBarTop * scrollHeight / viewHeight
+        }
     };
-    const onMouseUpBar = ()=>{
-        draggingRef.current =false;
+    const onMouseUpBar = () => {
+        draggingRef.current = false;
         console.log("end");
     };
-    useEffect(()=>{
-        document.addEventListener('mouseup',onMouseUpBar)
-        document.addEventListener('mousemove',onMouseMoveBar);
-        return ()=>{
-            document.removeEventListener('mouseup',onMouseUpBar)
-            document.removeEventListener('mousemove',onMouseMoveBar)
+    useEffect(() => {
+        document.addEventListener('mouseup', onMouseUpBar)
+        document.addEventListener('mousemove', onMouseMoveBar);
+        return () => {
+            document.removeEventListener('mouseup', onMouseUpBar)
+            document.removeEventListener('mousemove', onMouseMoveBar)
         }
-    },[])
+    }, [])
 
-    return(
+    return (
         <div className="fui-scroll" {...rest}>
-             {/*// onMouseDown={onMouseDownBar}*/}
-             {/*// onMouseMove={onMouseMoveBar}*/}
+            {/*// onMouseDown={onMouseDownBar}*/}
+            {/*// onMouseMove={onMouseMoveBar}*/}
 
             <div className="fui-scroll-inner"
                  onScroll={onScroll}
-                 ref = {containerRef}>
+                 ref={containerRef}>
                 {children}
             </div>
             <div className="fui-scroll-track">
-                <div className="fui-scroll-bar" style={{height:barHeight, transform:`translateY(${barTop}px)`}}
+                <div className="fui-scroll-bar" style={{height: barHeight, transform: `translateY(${barTop}px)`}}
                      onMouseDown={onMouseDownBar}
                 >
 

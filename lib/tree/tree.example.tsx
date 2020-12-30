b80f34c6d3dac155713e33faa217562f162797f2
richard1230 <yongjinhu1230@gmail.com>
2020-12-30 23:56:31 +0800

    让 tree 支持 multiple 联合类型: type A = {selected:string[],multiple:true} type B = {selected: string,multiple:false}
    type Props  = {
        sourceData: SourceDataItem[];
        onChange: (item: SourceDataItem, bool: boolean) => void;
    } & (A | B)

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -1,51 +1,53 @@
 import React, {useState} from "react";
 import Tree, {SourceDataItem} from "./tree";
 
-const TreeExample: React.FC = (props)=>{
+const TreeExample: React.FC = (props) => {
     const [array] = useState([
         {
-            text:'1',
-            value:'1',
-            children:[
+            text: '1',
+            value: '1',
+            children: [
                 {
-                    text:'1.1',value:'1.1',
-                     children:[
-                         {text:'1.1.1',value:'1.1.1'},
-                         {text:'1.1.2',value:'1.1.2'}
+                    text: '1.1', value: '1.1',
+                    children: [
+                        {text: '1.1.1', value: '1.1.1'},
+                        {text: '1.1.2', value: '1.1.2'}
 
-                     ]
+                    ]
                 },
                 {
-                    text:'1.2',value:'1.2',
+                    text: '1.2', value: '1.2',
                 }
             ]
-        },{
-            text:'2',
-            value:'2',
-            children:[
-                {text:'2.1',value:'2.1'},
-                {text:'2.2',value:'2.2'}
+        }, {
+            text: '2',
+            value: '2',
+            children: [
+                {text: '2.1', value: '2.1'},
+                {text: '2.2', value: '2.2'}
             ]
 
         }
     ]);
-    const [selectedValues,setSelectedValues] = useState(['1.1.1','1.1.2'])
-    const onChange = (item:SourceDataItem,bool:boolean)=>{
-        if (bool){
-            setSelectedValues([...selectedValues,item.value])
-        }else {
+    const [selectedValues, setSelectedValues] = useState(['1.1.1', '1.1.2'])
+    const onChange = (item: SourceDataItem, bool: boolean) => {
+        if (bool) {
+            setSelectedValues([...selectedValues, item.value])
+        } else {
             //保留没有被选中的，即:将选中的删掉
-           setSelectedValues(selectedValues.filter(value => value!==item.value))
+            setSelectedValues(selectedValues.filter(value => value !== item.value))
         }
     }
-    return(
+    return (
         <div>
             Tree
             <h1>展示数据</h1>
-            <div style={{width:200}}>
-                <Tree  sourceData={array}
-                       onChange={onChange}
-                       selectedValues={selectedValues}/>
+            <div style={{width: 200}}>
+                <Tree sourceData={array}
+                      onChange={onChange}
+                      selected={selectedValues}
+                      multiple={true}
+                />
             </div>
         </div>
     )
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -2,58 +2,72 @@ import React from "react";
 import {scopedClassMaker} from "../helpers/classes";
 import './tree.scss'
 
- export interface SourceDataItem {
+export interface SourceDataItem {
     text: string;
-    value:string;
+    value: string;
     children?: SourceDataItem[];
 }
 
-interface Props {
-    sourceData: SourceDataItem[],
-    selectedValues:string[],
-    onChange:(item:SourceDataItem,bool:boolean)=>void
-}
+type A = {selected:string[],multiple:true}
+type B = {selected: string,multiple:false}
+
+type Props  = {
+    sourceData: SourceDataItem[];
+    onChange: (item: SourceDataItem, bool: boolean) => void;
+} & (A | B)
+
+
+
 
 const scopedClass = scopedClassMaker('fui-tree');
 const sc = scopedClass;
 
-const renderItem = (item:SourceDataItem,
-                    selectValues: string[],
-                    onChange:(item:SourceDataItem,bool:boolean)=>void,
-                    level = 1)=>{
+const renderItem = (item: SourceDataItem,
+                    selected: string[],
+                    onChange: (item: SourceDataItem, bool: boolean) => void,
+                    level = 1) => {
 
     const classes = {
-        ['level-'+level]:true,
-        'Item':true
+        ['level-' + level]: true,
+        'Item': true
     };
-    return(
+    return (
         <div key={item.value}
              className={sc(classes)}>
-           <div className={sc('text')}>
-               <input type="checkbox"
-                      onChange={(e)=>onChange(item,e.target.checked)}
-                      checked={selectValues.indexOf(item.value)>=0}
-               />
-               {item.text}
-           </div>
-            {item.children?.map(sub=>{
-                return renderItem(sub,selectValues, onChange,level+1)
+            <div className={sc('text')}>
+                <input type="checkbox"
+                       onChange={(e) => onChange(item, e.target.checked)}
+                       checked={selected.indexOf(item.value) >= 0}
+                />
+                {item.text}
+            </div>
+            {item.children?.map(sub => {
+                return renderItem(sub, selected, onChange, level + 1)
             })}
         </div>
     )
 }
 
-const Tree: React.FC<Props> = (props)=>{
+const Tree: React.FC<Props> = (props) => {
 
-    return(
-        <div>
-            {
-                props.sourceData?.map(item=>{
-                    return  renderItem(item,props.selectedValues,props.onChange)
-                })
-            }
-        </div>
-    )
+    if (props.multiple) {
+        return (
+            <div>
+                {
+                    props.sourceData?.map(item => {
+                        return renderItem(item, props.selected, props.onChange)
+                    })
+                }
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                未完成
+            </div>
+        )
+    }
+
 }
 
 export default Tree

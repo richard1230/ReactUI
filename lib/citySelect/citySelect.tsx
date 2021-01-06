c74d9e034950929a92193ac5268db7b44b87bf24
richard1230 <yongjinhu1230@gmail.com>
2021-01-06 14:50:49 +0800

    显示当前城市

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from 'react-dom';
 
 interface Props {
@@ -25,8 +25,45 @@ const Dialog:React.FC<{onClose:()=>void}>=(props)=>{
     return ReactDOM.createPortal(
         (<div className="fui-citySelect-dialog"
               onClick={props.onClose}
-        >弹出内容</div>),document.body)
+        >
+            <header>
+                <span className="icon">&lt;</span>
+                <span>城市选择</span>
+            </header>
+            {/*<div className="currentCity">*/}
+            {/*    上海*/}
+            {/*</div>*/}
+            <CurrentLocation/>
+            <h2>全部城市</h2>
+            <div className="cityIndex">ABCDE...</div>
+            <div className="cityList">所有城市</div>
+        </div>),document.body)
 
+}
+
+
+const CurrentLocation:React.FC = ()=>{
+  const [city,setCity]=useState<string>('加载中...')
+    useEffect(()=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('get','http://ip-api.com/json/?lang=zh-CN')
+        xhr.onload=()=>{
+            const string = xhr.responseText;
+            const obj = JSON.parse(string);
+            const newcity = obj.city;
+            console.log(newcity);
+            setCity(newcity)
+        }
+        xhr.onerror = () => {
+            setCity('未知');
+        };
+        xhr.send()
+    },[]);
+    return (
+        <div className="currentCity">
+            当前城市：{city}
+        </div>
+    );
 }
 
 export default CitySelect
